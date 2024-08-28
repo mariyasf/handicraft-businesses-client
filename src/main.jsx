821125ceeb267fb571381e7b0d561125d4220938
mariyasf <mariasultanafahmida@gmail.com>
2024-08-28 23:06:20 +0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Router/Router.jsx";
 import AuthProvider from "./Provider/AuthProvider.jsx";
 createRoot(document.getElementById("root")).render(
+  
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
